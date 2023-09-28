54bee4b89a1edb7973751217bfcb25ed19cc44b1
“Daniel-Notice” <danielnoticeworkv2@gmail.com>
2023-09-28 17:34:10 +0100

    added javascript to the project

---
+++ assets/script.js
@@ -0,0 +1,28 @@
+
+
+/*
+if (topic === "HTML"){
+    console.log("Let's study HTML!")
+} else if (topic === "CSS"){
+    console.log("Let's study CSS!")
+} else if(topic === "Git"){
+    console.log("Let's study Git!")
+} else if (topic === "Javascript"){
+    console.log("Let's study Javascript!")
+} else {
+    console.log("Please try again!")
+}
+*/
+
+
+var shapes = ["triangle", "square", "pentagon", "circle"];
+console.log(shapes[0]);
+
+for(var x = 0; x < shapes.length; x++){
+    console.log(shapes[x])
+}
+
+var topics = ["HTML", "CSS", "Git", "Javascript"];
+for(var y = 0; y < topics.length; y++){
+    console.log(topics[y])
+}
